453cdc39a45a1bd3015588f0119d74163adfe4c9
zerolength <zerolengthisnotaccessible@gmail.com>
2023-10-19 11:22:13 -0700

    Update cleaner.sh

--- ans/cleaner.sh
+++ ans/cleaner.sh
@@ -9,5 +9,30 @@ for namespace in $namespaces; do
     echo "Deleted namespace: $namespace"
 done
 
+
+# Specify the list of link names to keep (space-separated)
+links_to_keep="lo ens3 docker0 vethb132fcc@if4 br-95097e7166ed veth39916af@if27 veth4b7e70c@if29 vethf36df0a@if31 veth62152f6@if33 vethb138804@if35"
+
+# Get a list of all network links
+all_links=$(ip link show | awk -F ": " '{print $2}')
+
+# Iterate through all links
+for link in $all_links; do
+    # Check if the current link is in the list of links to keep
+    if [[ $links_to_keep == *"$link"* ]]; then
+        echo "Keeping link: $link"
+    else
+        # Delete the link
+        ip link delete "$link"
+        echo "Deleted link: $link"
+    fi
+done
+
+echo "Network links have been cleaned."
+
+
+
+
+
 echo "All network namespaces have been removed."
 
